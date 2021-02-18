6fb86e1d3e650a5570e384cfdd94e1b02a9bc9bb
AntoineVen <ant.venturelli06@gmail.com>
2021-02-18 18:41:49 +0100

    #3 Update Move

--- src/main/java/Labyrinthe/Move.java
+++ src/main/java/Labyrinthe/Move.java
@@ -5,7 +5,7 @@ public enum Move {
     //Axe ordonnée orienté en haut
     UP(0, 1), DOWN(0, -1), LEFT(-1, 0), RIGTH(1, 0);
 
-    public int x, y;
+    private int x, y;
 
     Move(int x, int y){
         this.x = x;
