010633a414fbc68c32f31f2d8f1fdf8f178e4d86
AntoineVen <ant.venturelli06@gmail.com>
2021-02-18 18:36:12 +0100

    #3 Update Move
    Premières implémentations mouvements.
    On se réfère aux orientations classiques des
    graphes. (je pense que c'est le plus intuitif)
    Axe abscisse orienté à droite
    Axe ordonnée orienté en haut

--- src/main/java/Labyrinthe/Move.java
+++ src/main/java/Labyrinthe/Move.java
@@ -1,5 +1,22 @@
 package Labyrinthe;
 
 public enum Move {
-    // TODO
+    //Axe abscisse orienté à droite
+    //Axe ordonnée orienté en haut
+    UP(0, 1), DOWN(0, -1), LEFT(-1, 0), RIGTH(1, 0);
+
+    public int x, y;
+
+    Move(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getMoveX(){
+        return x;
+    }
+
+    public int getMoveY() {
+        return y;
+    }
 }
