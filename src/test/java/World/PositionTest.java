2d0f6c0787e9e8bee54c62d7f0af7b28b44225d1
Fissore23 <fisso9900@gmail.com>
2021-02-19 17:28:55 +0100

    #3 - Modified Position Test

--- src/test/java/World/PositionTest.java
+++ src/test/java/World/PositionTest.java
@@ -8,9 +8,12 @@ public class PositionTest {
 
     @Test
     public void testLucky() {
+        World w = new World();
         Position p0 = new Position(0, 0);
-        Position p1 = new Position(0, 0);
-        assertEquals(p1, p0);
+        Move m = Move.UP;
+        p0.nextPosition(w, m.getMove());
+        assertEquals(0, p0.getX());
+        assertEquals(1, p0.getY());
     }
 
 }
