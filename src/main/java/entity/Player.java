09bae003c2ff05a53819654c9fe747c69e46b5c5
AntoineVen <ant.venturelli06@gmail.com>
2021-02-26 00:41:51 +0100

    #11 Update of Player
    Automatic universalization of keyboard commands

--- src/main/java/entity/Player.java
+++ src/main/java/entity/Player.java
@@ -5,7 +5,9 @@ import graphics.elements.Position;
 import graphics.map.WorldMap;
 import items.AbstractItem;
 
+import java.awt.im.InputContext;
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -45,44 +47,66 @@ public class Player extends AbstractEntity {
 
     public static void gamePlayer(){
         Scanner sc = new Scanner(System.in);
+        String buffer;
         char key;
         boolean gameState = true;
         WorldMap wmap = WorldMap.getInstanceWorld();
+
         while (gameState){
-            key = sc.nextLine().charAt(0);
-            Position pos = instancePlayer.getPos();
-            Position oldPos = new Position(pos.getX(), pos.getY());
-            switch (key){
-                case 'z': pos.nextPosition(wmap, Move.UP.getMove());
-                    System.out.println("Hero has moved to UP");//just for check
-                    break;
-                case 'q': pos.nextPosition(wmap, Move.LEFT.getMove());
-                    System.out.println("Hero has moved to LEFT");
-                    break;
-                case 's': pos.nextPosition(wmap, Move.DOWN.getMove());
-                    System.out.println("Hero has moved to BOTTOM");
-                    break;
-                case 'd': pos.nextPosition(wmap, Move.RIGHT.getMove());
-                    System.out.println("Hero has moved to RIGHT");
-                    break;
-                case 'p': gameState = false;
-                    System.out.println("Leaved game");
-                    break;
-                default: break;
-            }
+            buffer = sc.nextLine();
+            if (buffer.length() == 1) {
+                key = buffer.charAt(0);
+                Position pos = instancePlayer.getPos();
+                Position oldPos = new Position(pos.getX(), pos.getY());
 
-            if (key == 'z' || key == 'q' || key == 's' || key == 'd'){
-                wmap.toEmptyACell(oldPos.getX(), oldPos.getY());
-                instancePlayer.setPosition(pos);
-                wmap.setPlayerPlace(pos.getX(), pos.getY());
-                wmap.repaint();
-            }
+                if(getKeyboard().equals("fr_FR")){ key = Player.charConverterToUniversal(key); }
 
+                switch (key) {
+                    case 'w': pos.nextPosition(wmap, Move.UP.getMove()); break;
+                    case 'a': pos.nextPosition(wmap, Move.LEFT.getMove()); break;
+                    case 's': pos.nextPosition(wmap, Move.DOWN.getMove()); break;
+                    case 'd': pos.nextPosition(wmap, Move.RIGHT.getMove()); break;
+                    case 'p':
+                        gameState = false;
+                        System.out.println("Leaved game");
+                        break;
+                    default:
+                        break;
+                }
+
+                if (key == 'w' || key == 'a' || key == 's' || key == 'd') {
+                    wmap.toEmptyACell(oldPos.getX(), oldPos.getY());
+                    instancePlayer.setPosition(pos);
+                    wmap.setPlayerPlace(pos.getX(), pos.getY());
+                    wmap.repaint();
+                }
+            }
         }
         sc.close();
     }
-    public static void showCommands(){
-        System.out.println("To move : z (top), q (left), s (bottom), d (right)" + System.lineSeparator()
-                         + "To leave : p");
+
+    public static String getKeyboard(){
+        InputContext context = InputContext.getInstance();
+        Locale country = context.getLocale();
+        return country.toString();
     }
+
+    public static void showCommands(){
+        char top = 'w', left = 'a';
+        if (getKeyboard().equals("fr_FR")){ top = 'z'; left = 'q'; }
+        System.out.printf("To move : %c (top), %c (left), s (bottom), d (right)%sTo leave : p", top, left, System.lineSeparator());
+    }
+
+    public static char charConverterToUniversal(char c){
+        switch (c){
+            case 'z': return 'w';
+            case 'q': return 'a';
+
+            case 'a':       //possibly to be defined
+            case 'w': return '_';
+            default: return c;
+        }
+    }
+
+
 }
