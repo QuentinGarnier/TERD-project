b9b0641c17b90ee2eddfb6f4e2395cf48d893c27
AntoineVen <ant.venturelli06@gmail.com>
2021-02-25 13:47:17 +0100

    test color

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import graphics.map.WorldMap;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(ColorStr.RED + "HELLO \033[0m");
+        System.out.println(ColorStr.RED + "HELLO" + ColorStr.WHITE + " WORLD");
 
         System.out.println(WorldMap.getInstanceWorld());
     }
