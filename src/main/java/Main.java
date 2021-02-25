df65bfc21ef9a6a506cf9ae595d5eca21a35b3df
AntoineVen <ant.venturelli06@gmail.com>
2021-02-25 13:44:48 +0100

    little update color

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,9 @@
+import graphics.ColorStr;
 import graphics.map.WorldMap;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\033[91mHELLO \033[0m");
+        System.out.println(ColorStr.RED + "HELLO \033[0m");
 
         System.out.println(WorldMap.getInstanceWorld());
     }
---
+++ src/main/java/graphics/ColorStr.java
@@ -0,0 +1,12 @@
+package graphics;
+
+public class ColorStr {
+    public static final String BLACK = "\033[30m";
+    public static final String RED = "\033[31m";
+    public static final String GREEN = "\033[32m";
+    public static final String YELLOW = "\033[33m";
+    public static final String BLUE = "\033[34m";
+    public static final String MAGENTA = "\033[35m";
+    public static final String CYAN = "\033[36m";
+    public static final String WHITE = "\033[37m";
+}
