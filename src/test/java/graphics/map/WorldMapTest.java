fa232d60389d22f696af8ad413f87c170151e160
Fissore23 <fisso9900@gmail.com>
2021-03-02 18:23:19 +0100

    #10 #20 Test on security of lab Verify that get cell do not modify the content of the World

--- src/test/java/graphics/map/WorldMapTest.java
+++ src/test/java/graphics/map/WorldMapTest.java
@@ -1,14 +1,16 @@
 package graphics.map;
 
 import graphics.elements.Room;
+import graphics.elements.cells.Cell;
+import graphics.elements.cells.CellElementType;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class WorldMapTest {
+    WorldMap w = WorldMap.getInstanceWorld();
     @Test
-    public void testProgramme() {
-        WorldMap w = WorldMap.getInstanceWorld();
+    public void testProgrammeStrongConnected() {
         for (int i = 0; i < 100; i++) {
             System.out.println();
             for (Room r : w.getRooms()) {
@@ -16,8 +18,26 @@ public class WorldMapTest {
             }
             w.generateWorld();
         }
-
         //System.out.println(WorldMap.getInstanceWorld().toString());
-
+    }
+    @Test
+    public void testSecurityCell(){
+        // test that lab do not modify
+        for (int i = 0; i < WorldMap.MAX_X; i++){
+            for (int j = 0; j < WorldMap.MAX_Y; j++){
+                Cell c = w.getCell(i, j);
+                c =  new Cell(CellElementType.OUTSIDE_ROOM, -1);
+            }
+        }
+        boolean allAtZero = true;
+        for (int i = 0; i < WorldMap.MAX_X; i++){
+            for (int j = 0; j < WorldMap.MAX_Y; j++){
+                if (w.getCell(i, j).getBaseContent() != CellElementType.OUTSIDE_ROOM){
+                    allAtZero = false;
+                    break;
+                }
+            }
+        }
+        assertFalse(allAtZero);
     }
 }
