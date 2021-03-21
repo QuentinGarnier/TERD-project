9cc08a0230ed7ff82b8d0e0424e69efed7c7fe12
Fissore23 <fisso9900@gmail.com>
2021-03-21 18:21:09 +0100

    #21 End now doesn't block the way to take a corridor

--- src/main/java/graphics/map/WorldMap.java
+++ src/main/java/graphics/map/WorldMap.java
@@ -49,7 +49,15 @@ public class WorldMap {
         Room room = rooms.get(iRoom);
         int x = /*Player.getInstancePlayer().getPosition().getX() + 1;*/ room.getTopLeft().getX() + rnd.nextInt(room.getWidth() - 1) + 1;
         int y = /*Player.getInstancePlayer().getPosition().getY();*/ room.getTopLeft().getY() + rnd.nextInt(room.getHeight() - 1) + 1;
+        Position pos = new Position(x, y);
         if (getCell(x, y).isAccessible() && getCell(x, y).getItem() == null) {
+            Position[] neighbors = pos.getNeighbor(false);
+            for (Position p : neighbors){
+                if (getCell(p).isDoor()) {
+                    placeEnd();
+                    return;
+                }
+            }
             AbstractItem.end.setPosition(new Position(x, y));
             getCell(x, y).setItem(AbstractItem.end);
             AbstractItem.end.setLocation();
