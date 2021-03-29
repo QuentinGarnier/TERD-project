1f4d9716d67ff692ec9412e8a5a0926f6d6597f4
QuentinGarnier <quentin.garnier@etu.unice.fr>
2021-03-29 17:06:53 +0200

    #38 : murs & ombres
    - Perfect walls
    - Shadows

--- src/main/java/graphics/window/GamePanel.java
+++ src/main/java/graphics/window/GamePanel.java
@@ -16,6 +16,7 @@ import java.util.List;
 
 public class GamePanel extends JPanel {
     private JPanel heroLabel = Player.getInstancePlayer();
+    private final ImageIcon shadow = new ImageIcon("data/images/map/miscellaneous/shadow.png");
     private final ImageIcon red = new ImageIcon("data/images/map/miscellaneous/square_red.png");
     private final ImageIcon green = new ImageIcon("data/images/map/miscellaneous/square_green.png");
     private final JLabel squareLabel = new JLabel(green);
@@ -70,14 +71,42 @@ public class GamePanel extends JPanel {
         // BASE CONTENT
         for(int x = 0; x < WorldMap.MAX_X; x++) for(int y = 0; y < WorldMap.MAX_Y; y++) {
             JLabel jLabel = new JLabel();
-            ImageIcon img;
+            ImageIcon img = null;
             Cell currentCell = worldMap.getCell(x, y);
             CellElementType currentCet = currentCell.getBaseContent();
             CellElementType obstacle = currentCell.getObstacle();
-            if(y < WorldMap.MAX_Y - 1 && currentCet.isWall()) {
-                CellElementType cet = worldMap.getCell(x, y + 1).getBaseContent();
-                img = new ImageIcon((cet.isWall() ? CellElementType.VERTICAL_WALL : currentCet).getIcon().getImage());
-            } else img = new ImageIcon(currentCet.getIcon().getImage());
+            if(y > 0 && currentCet == CellElementType.HORIZONTAL_WALL) {
+                CellElementType previous = worldMap.getCell(x, y - 1).getBaseContent();
+                if (previous.isWall()) img = new ImageIcon(CellElementType.CORNER_BOT.getIcon().getImage());
+                else if (y < WorldMap.MAX_Y - 1) {
+                    CellElementType after = worldMap.getCell(x, y + 1).getBaseContent();
+                    img = new ImageIcon((after.isWall() ? CellElementType.VERTICAL_WALL : currentCet).getIcon().getImage());
+                }
+            }
+            else if(currentCet == CellElementType.CORNER_BOT) {
+                if (y < WorldMap.MAX_Y - 1) {
+                    CellElementType cet = worldMap.getCell(x, y + 1).getBaseContent();
+                    img = new ImageIcon((cet.isWall() ? CellElementType.VERTICAL_WALL : currentCet).getIcon().getImage());
+                }
+            }
+            else if(currentCet == CellElementType.CORNER_TOP) {
+                if (y > 0) {
+                    CellElementType cet = worldMap.getCell(x, y - 1).getBaseContent();
+                    img = new ImageIcon((cet.isWall() ? CellElementType.VERTICAL_WALL : currentCet).getIcon().getImage());
+                }
+            }
+            if (img == null) img = new ImageIcon(currentCet.getIcon().getImage());
+
+            if (x > 0 && y > 0) {
+                CellElementType leftC = worldMap.getCell(x - 1, y).getBaseContent();
+                CellElementType topLeftC = worldMap.getCell(x - 1, y - 1).getBaseContent();
+                if(leftC.isWall() && !currentCet.isWall() && topLeftC.isWall()) {
+                    JLabel shad = new JLabel(shadow);
+                    shad.setBounds(x * size, y*size, size, size);
+                    add(shad);
+                }
+            }
+
             if (obstacle != null) {
                 JLabel obstacleL = new JLabel(new ImageIcon(obstacle.getIcon().getImage()));
                 obstacleL.setBounds(x * size, y * size, size, size);
