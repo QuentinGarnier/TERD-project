211a457c443ea4c0750ef7ae46e3958c890f675b
Fissore23 <fisso9900@gmail.com>
2021-04-13 10:50:02 +0200

    #21 bug sur l'emplacement de la cible au moment de la création d'un nouveau World

--- src/main/java/graphics/ChooseAttackCell.java
+++ src/main/java/graphics/ChooseAttackCell.java
@@ -8,10 +8,16 @@ import graphics.elements.cells.CellElementType;
 import graphics.map.WorldMap;
 
 public class ChooseAttackCell {
-    private static final Player hero = Player.getInstancePlayer();
-    private static final WorldMap worldMap = WorldMap.getInstanceWorld();
+    private static Player hero;
+    private static WorldMap worldMap;
+
+    private static void update(){
+        hero = Player.getInstancePlayer();
+        worldMap = WorldMap.getInstanceWorld();
+    }
 
     public static Position selectCase(Position pos, Move m){
+        update();
         if (pos == null) return selectCase();
         if (m != null){
             Cell cell = worldMap.getCell(pos);
@@ -29,12 +35,14 @@ public class ChooseAttackCell {
     }
 
     public static Position selectCase(){
+        update();
         Cell cell = worldMap.getCell(hero.getPosition());
         cell.setAimed(true);
         return hero.getPosition();
     }
 
     public static void unselectCase(Position p){
+        update();
         worldMap.getCell(p).setAimed(false);
     }
 }
