f083146d48210850afd0854261063d6fbbe418ce
Fissore23 <fisso9900@gmail.com>
2021-04-28 12:44:58 +0200

    #22 Inventory upgrade Mis à jour les boutons de l'inventaire graphiquement + remise à zéro du bouton B1 si on clique après sur le bouton B2 de l'inv

--- src/main/java/graphics/window/InventoryPanel.java
+++ src/main/java/graphics/window/InventoryPanel.java
@@ -9,8 +9,12 @@ import items.collectables.ItemEquip;
 import items.collectables.ItemFood;
 
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -22,11 +26,13 @@ public class InventoryPanel extends JPanel {
     private final JLabel miniLog;
     private final JScrollPane scrollPane;
     private final GridLayout gl = new GridLayout(4,1);
+    private final List<myButton> buttons;
 
     private InventoryPanel(){
         setLayout(new BorderLayout());
         miniLog = new JLabel();
         contents = new JPanel(gl);
+        buttons = new ArrayList<>();
         scrollPane = new JScrollPane(contents, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         miniLog.setHorizontalAlignment(SwingConstants.CENTER);
         setInventoryText();
@@ -38,19 +44,19 @@ public class InventoryPanel extends JPanel {
         String s1 = ai.toString(), s2 = ai.getEffect();
         int s3 = ai.getPrice();
         JPanel panel = new JPanel(new BorderLayout());
-        JLabel fstCol = createLog(s1, Color.GRAY);
+        JLabel fstCol = createLog(s1, Color.BLACK);
         fstCol.setForeground(ai.equals(Player.getInstancePlayer().getDefenceItem()) ||
-                ai.equals(Player.getInstancePlayer().getAttackItem())? Color.GREEN : Color.GRAY);
-        JLabel sndCol = createLog(s2, Color.GRAY);
-        JLabel thrCol = createLog((s3 < 10 ? " " : "") + s3 + " $", Color.GRAY);
+                ai.equals(Player.getInstancePlayer().getAttackItem())? Color.GREEN : Color.BLACK);
+        JLabel thrCol = createLog((s3 < 10 ? " " : "") + s3 + " $", Color.BLACK);
 
         fstCol.setHorizontalAlignment(SwingConstants.LEFT);
-        sndCol.setHorizontalAlignment(SwingConstants.CENTER);
         thrCol.setHorizontalAlignment(SwingConstants.RIGHT);
 
         panel.add(fstCol, BorderLayout.WEST);
         panel.add(thrCol, BorderLayout.EAST);
         myButton jButton = new myButton(ai, panel);
+        myAddMouseListener(jButton);
+        buttons.add(jButton);
         contents.add(jButton);
     }
 
@@ -94,16 +100,35 @@ public class InventoryPanel extends JPanel {
             setFocusable(false);
             this.panel = panel;
             this.ai = ai;
+            panel.setOpaque(false);
             add(panel);
             ActionListener al = e -> {
                 if (Player.getInstancePlayer().getHP() == 0) return;
+                setBorder(null);
                 revalidate();
                 removeAll();
                 add(new choiceButton(ai));
+                restoreOtherButtons();
             };
             ToolTipManager.sharedInstance().setInitialDelay(0);
             setToolTipText(ai.getDescription());
             super.addActionListener(al);
+        }
+
+        public void restoreContent(){
+            if (ai instanceof ItemEquip){
+                ItemEquip ie = (ItemEquip) ai;
+                panel.getComponent(0).setForeground(ie.isEquipped() ? Color.GREEN : Color.BLACK);
+            }
+            removeAll();
+            add(panel);
+            repaint();
+        }
+
+        private void restoreOtherButtons(){
+            buttons.forEach(b -> {
+                if (!b.ai.equals(this.ai)) b.restoreContent();
+            });
         }
 
         @Override
@@ -112,25 +137,35 @@ public class InventoryPanel extends JPanel {
         }
     }
 
+    private static void addBorder(JButton button){
+        button.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createCompoundBorder(
+                        BorderFactory.createBevelBorder(BevelBorder.RAISED, Tools.WindowText.golden, Tools.WindowText.dark_golden),
+                        BorderFactory.createLineBorder(Tools.WindowText.golden, 2)),
+                BorderFactory.createLineBorder(new Color(140, 110, 70))));
+    }
 
+    public static void myAddMouseListener(JButton button){
+        addBorder(button);
+        button.addMouseListener(new MouseAdapter() {
+            Color bg = button.getBackground();
+            Color hoverBG = new Color(180, 150, 110);
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                button.setBackground(hoverBG);
+            }
+            @Override
+            public void mouseExited(MouseEvent e) {
+                button.setBackground(bg);
+            }
+        });
+    }
 
     private class choiceButton extends JPanel {
-        private void addMouseListener(JButton button){
-            button.setBorder(null);
-            button.addMouseListener(new MouseAdapter() {
-                Color bg = button.getBackground();
-                Color hoverBG = new Color(180, 150, 110);
-                @Override
-                public void mouseEntered(MouseEvent e) {
-                    button.setBackground(hoverBG);
-                }
-                @Override
-                public void mouseExited(MouseEvent e) {
-                    button.setBackground(bg);
-                }
-            });
-        }
+        private final AbstractCollectableItem ai;
+
         public choiceButton(AbstractCollectableItem ai) {
+            this.ai = ai;
             setLayout(new GridLayout(0,3));
             Player player = Player.getInstancePlayer();
             JButton equip = new JButton((ai instanceof ItemEquip) ?
@@ -138,9 +173,9 @@ public class InventoryPanel extends JPanel {
                     Language.buttonConsume());
             JButton throwAway = new JButton(Language.buttonThrow());
             JButton esc = new JButton(Language.back());
-            addMouseListener(equip);
-            addMouseListener(throwAway);
-            addMouseListener(esc);
+            InventoryPanel.myAddMouseListener(equip);
+            InventoryPanel.myAddMouseListener(throwAway);
+            InventoryPanel.myAddMouseListener(esc);
             equip.addActionListener(e -> {
                 equipping(ai, ai.use());
                 if(GameWindow.hasSound()) Tools.play(Objects.requireNonNull(getClass().getClassLoader().getResource("data/audio/SE/" + ai.getSE() + ".wav")), false);
